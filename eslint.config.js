9abdfb18f894cbb98193d89ceeb1a89f1f861df7
Teroths <Teroths@users.noreply.github.com>
2025-08-20 14:20:47 +0000

    deploy: 0d8671b9fcf67de9f7286f3e9415a20b1fef036f

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+const globals = require('globals');
+
+module.exports = [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+    },
+  },
+];
