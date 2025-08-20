df19d11cda7826da4abf1660e73d2266c064b4fb
Teroths <thomas.menet77@gmail.com>
2025-08-20 11:34:29 +0200

    Ajout d'un nouveau CI/CD

---
+++ eslint.cnfig.js
@@ -0,0 +1,17 @@
+const globals = require('globals');
+
+module.exports = [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+    },
+    rules: {
+    },
+  },
+];
